bff5ac11a5f43e9f8062efcce94834cca188f3cf
Lucas Dreher <lucasdreher@gmail.com>
2022-03-06 16:43:32 +0100

    Create SkillsAll component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SkillsAll />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SkillsAll from "./components/SkillsAll.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    SkillsAll,
+  },
+};
 </script>
 
 <style>
