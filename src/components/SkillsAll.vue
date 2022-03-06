cd8c2bcaec948823741cf61c6e216558456b9d0b
Lucas Dreher <lucasdreher@gmail.com>
2022-03-06 17:07:19 +0100

    Component SkillsAll has content

--- src/components/SkillsAll.vue
+++ src/components/SkillsAll.vue
@@ -1,11 +1,25 @@
 <template>
-  <div class="hello">SkillsAll</div>
+  <div class="hello">
+    <div class="holder">
+      <ul>
+        <li v-for="(data, index) in skills" :key="index">
+          {{ index }}. {{ data.skill }}
+        </li>
+      </ul>
+      <p v-if="skills.length > 1">You have more than 1 skill.</p>
+      <p v-else>You have less than or equal to 1 skill.</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "SkillsAll",
-  props: {},
+  data() {
+    return {
+      skills: [{ skill: "Vue.js" }, { skill: "Frontend developer" }],
+    };
+  },
 };
 </script>
 
